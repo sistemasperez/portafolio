b0797b28f66d05a7326327f334fd3b5f9a5c506b
Anibal Perez <anibalw23@hotmail.com>
2025-06-29 08:28:48 -0400

    Initial portfolio commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tu-repositorio-nombre/', // Cambia esto por el nombre de tu repositorio
+  base: '/portafolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
